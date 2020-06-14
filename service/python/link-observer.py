6e26f0afd410d977c1aee575b1af4ab173d85492
Geoff White <geoffw@durganetworks.com>
2020-06-14 08:43:52 -0700

    minor change to add variable name for monitor port

--- service/python/link-observer.py
+++ service/python/link-observer.py
@@ -6,6 +6,7 @@ import requests
 urls = ['https://httpstat.us/503','https://httpstat.us/200']
 
 # Create a metric to track time spent and requests made.
+MONITOR_PORT= 8001
 REQUEST_SUMMARY = Summary('lo_external_url_response_ms', 'Time spent processing request',['url'])
 RESPONSE_GAUGE = Gauge('lo_external_url_up', 'up = 1 all else 0', ['url'])
 
@@ -16,7 +17,7 @@ def process_request(url):
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
-    start_http_server(8001)
+    start_http_server(MONITOR_PORT)
     # Generate some requests.
     while True:
         time.sleep(1)
