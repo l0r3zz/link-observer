d8c2258c11eefd98b2af5fd1244d26c28b9e5fbf
Geoff White <geoffw@durganetworks.com>
2020-06-15 18:00:44 -0700

    First working version of golang version of link-observer

--- service/golang/link-observer.go
+++ service/golang/link-observer.go
@@ -1,17 +1,20 @@
-// NOTE: This is not yet functional!
 package main
 
 import (
-	"fmt"
+	"flag"
+	//	"fmt"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
 	"net/http"
 	//	"time"
 )
 
 var (
-	url             = [...]string{"https://httpstat.us/200", "https://httpstat.us/503"}
-	REQUEST_SUMMARY = prometheus.NewSummaryVec(prometheus.SummaryOpts{
+	listeningAddress = flag.String("telemetry.address", "localhost:8003", "Address on which to expose metrics.")
+	metricsEndpoint  = flag.String("telemetry.endpoint", "/metrics", "Path under which to expose metrics.")
+	url              = [...]string{"https://httpstat.us/200", "https://httpstat.us/503"}
+	REQUEST_SUMMARY  = prometheus.NewSummaryVec(prometheus.SummaryOpts{
 		Name: "link_observer_url_response_ms",
 		Help: "Agrigate Response time for URL probe",
 	}, []string{"url"})
@@ -22,28 +25,34 @@ var (
 )
 
 func process_request(url string) int {
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Printf("Get failed to %s\n", url)
-		return -1
-	}
+	timer := prometheus.NewTimer(REQUEST_SUMMARY.WithLabelValues(url))
+	defer timer.ObserveDuration()
+	resp, _ := http.Get(url)
 	defer resp.Body.Close()
 	return resp.StatusCode
 }
 
-func main() {
-	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(":8003", nil)
+func probeSites() {
 	var response_code int
-	fmt.Printf("Got  to %s\n", url)
-	for _, site := range url {
-		//		time.Sleep(1)
-		response_code = process_request(site)
-		if response_code == 200 {
-			fmt.Printf("OK")
-		} else {
-			fmt.Printf("BAD")
-		}
+	prometheus.Register(REQUEST_SUMMARY)
+	prometheus.Register(RESPONSE_GAUGE)
+	go func() {
+		for _, site := range url {
+			//		time.Sleep(1)
+			response_code = process_request(site)
+			if response_code == 200 {
+				RESPONSE_GAUGE.WithLabelValues(site).Set(1)
+			} else {
+				RESPONSE_GAUGE.WithLabelValues(site).Set(0)
+			}
 
-	}
+		}
+	}()
+}
+func main() {
+	flag.Parse()
+	probeSites()
+	http.Handle(*metricsEndpoint, promhttp.Handler())
+	log.Fatal(http.ListenAndServe(*listeningAddress, nil))
+
 }
